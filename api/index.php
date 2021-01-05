7136170bea7834c30cdb0fc2bc455a3cef529c74
Ari Jesus <arimario.jesus@hotmail.com>
2021-01-05 17:32:27 -0300

    feat: init api

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// HEADER
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+use App\Controller\Players;
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
